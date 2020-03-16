5793c34d5ec732be527767f4228c1124134c9870
li <mysql9688@163.com>
2020-03-16 16:27:50 +0800

    初始化项目

---
+++ Swoft_LiveEc/test/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+use SwoftTest\Testing\TestApplication;
+
+$baseDir = dirname(__DIR__);
+$vendor  = dirname(__DIR__) . '/vendor';
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader->addPsr4('SwoftTest\\Testing\\', $vendor . '/swoft/framework/test/testing/');
+
+$application = new TestApplication([
+    'basePath' => $baseDir,
+]);
+$application->setBeanFile($baseDir . '/app/bean.php');
+$application->run();
